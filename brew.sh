edc9908156f37b8d32753b9fb672b0ae19702672
Xi Xiao <xi.xiao@biit.fi>
2018-03-22 23:24:15 +0200

    _

--- brew.sh
+++ brew.sh
@@ -64,8 +64,9 @@ grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
+brew install grep
+brew install openssh
+brew install tmux
 # brew install homebrew/dupes/screen
 # brew install homebrew/php/php55 --with-gmp
 
